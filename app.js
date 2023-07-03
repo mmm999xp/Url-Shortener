889d6f97d8e9b718c7b1cdd111fd4e29b3ff9ebb
mmm999xp <mmm999xp@gmail.com>
2023-07-03 10:49:03 +0800

    add mongoose ,mongodb connect finish

--- app.js
+++ app.js
@@ -1,4 +1,25 @@
 const express = require('express')
+const mongoose = require('mongoose')
+
+if(process.env.NODE_ENV !== 'production'){
+  require('dotenv').config()
+}
+
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+
+const db = mongoose.connection
+db.on('error' , ()=>{
+  console.log('MongoDB Error !')
+})
+db.once('open' , ()=>{
+  console.log('MongoDB Connecrted !')
+})
+
+
+
+
+
+
 const app = express()
 
 app.get('/', (req,res)=>{
